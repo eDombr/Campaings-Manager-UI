8c7732489c715f768415d583d78f1e092ca35425
Eugene Dombrovsky <eugene.dombrowsky@gmail.com>
2017-10-20 16:01:44 +0300

    moved header, setuping

--- client/app/services/campaign/campaign.service.js
+++ client/app/services/campaign/campaign.service.js
@@ -4,26 +4,26 @@ class CampaignService {
 
         this._$http = $http;
 
-        this.baseUrl = 'https://5cd3f999-f49f-4e42-8b8b-173c7185f093.mock.pstmn.io'
+        this.baseUrl = 'https://5cd3f999-f49f-4e42-8b8b-173c7185f093.mock.pstmn.io/campaigns'
     }
 
     getCampaigns() {
-        return this._$http.get(`${this.baseUrl}/campaigns`)
+        return this._$http.get(`${this.baseUrl}`)
             .then(result => {
                 return result.data;
             });
     }
 
     activateStatus(id) {
-        return this._$http.post(`${this.baseUrl}/campaigns/${id}/activate`)
+        return this._$http.post(`${this.baseUrl}/${id}/activate`)
     }
 
     deactivateStatus(id) {
-        return this._$http.post(`${this.baseUrl}/campaigns/${id}/deactivate`)
+        return this._$http.post(`${this.baseUrl}/${id}/deactivate`)
     }
 
     getCampaignStats(id) {
-        return this._$http.get(`${this.baseUrl}/campaigns/${id}/stats`)
+        return this._$http.get(`${this.baseUrl}/${id}/stats`)
             .then(result => {
                 return result.data;
             })
